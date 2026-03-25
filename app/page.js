e2a0a4e797d174a6ceb39a51f8d5b0824727344f
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2026-03-25 10:11:28 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
         <p>The chatbot on this page has been implemented using an iframe.</p><br />
       </div>
 
-      <Script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1" strategy="lazyOnload" />
+      <Script src="https://console.dialogflow.com/api-client/demo/embedded/0de4163b-eaa6-47fa-a782-c9503d2ee29e" strategy="lazyOnload" />
       <df-messenger
         intent="WELCOME"
         chat-title="XM Bot"
